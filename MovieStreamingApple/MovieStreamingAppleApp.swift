e0afb91711fd28425f13431628399195d3e5546a
leanhdoan <la.doan2011@gmail.com>
2026-03-24 18:09:16 +0700

    Initial Commit

---
+++ MovieStreamingApple/MovieStreamingAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStreamingAppleApp.swift
+//  MovieStreamingApple
+//
+//  Created by Lê Anh Đoàn on 24/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieStreamingAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
